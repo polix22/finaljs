de82348c4f701776bb554429f6f752dd4f8f258b
HipolitoGodoy <godoyhipolito0@gmail.com>
2024-01-05 19:18:53 -0300

    segundo commit

--- script.js
+++ script.js
@@ -6,40 +6,43 @@ fetch('divisas.json')
     })
     .then((data) => {
         divisas = data;
-        //console.log(divisas);
         const boton = document.getElementById("botonconvertir");
         boton.addEventListener("click", function(event) {
             event.preventDefault();
-
             const divisaOrigen = document.getElementById('DivisaOrigen').value;
             const cantidad = parseFloat(document.getElementById('Cantidad').value);
             const divisaDestino = document.getElementById('DivisaDestino').value;
-
+//verifico que la variable esta definida
             const divisaOrigenEncontrada = divisas.find(divisa => divisa.nombre === divisaOrigen);
             const divisaDestinoEncontrada = divisas.find(divisa => divisa.nombre === divisaDestino);
-
+            
             if (divisaOrigenEncontrada && divisaOrigenEncontrada.precio !== undefined &&
                 divisaDestinoEncontrada && divisaDestinoEncontrada.precio !== undefined) {
                 
                 const divisaOrigenPrecio = divisaOrigenEncontrada.precio;
                 const divisaDestinoPrecio = divisaDestinoEncontrada.precio;
-
-                // conversión por regla de 3 simples
+ // conversión por regla de 3 simples
                 const resultado = (cantidad * divisaOrigenPrecio) / divisaDestinoPrecio;
-
                 const resultadoElemento = document.getElementById("resultado");
+//modifico el dom con el resultado y creo un sweetalert mostrando el resultado
                 resultadoElemento.textContent = `Resultado de la conversión: ${resultado} ${divisaDestino}`;
+                Swal.fire({
+                    imageUrl:"./img/bien.jpg",
+                    title: `${divisaOrigen}${cantidad}=${divisaDestino}${resultado}`,
+                    footer: 'Lionel hutz'
+                  });
             } else {
-                // Manejo de caso en el que la divisa no se encuentra
-                console.error('La divisa de origen o destino no fue encontrada o no tiene un precio definido');
+// Manejo de caso en el que la divisa no se encuentra con un sweet alert
+                Swal.fire({
+                    imageUrl:"./img/mal.jpg",
+                    title: "CAMPO VACIO",
+                    text: "verifique el monto y las divisas",
+                    footer: "Lionel hutz"
+                  });
             }
         });
     })
-    .catch(error => {
-        console.error('Error al obtener los datos de divisas:', error);
-    });
-
- //storage no supe como aplicar el local storage en mi pagina, guarde los precios de las divisas en pesos del dia anterior 
+//storage no supe como aplicar el local storage en mi pagina, guarde los precios de las divisas en pesos del dia anterior 
  localStorage.setItem("🍌", 500);
  localStorage.setItem("USD$", 1100);
  localStorage.setItem("€", 1600);
